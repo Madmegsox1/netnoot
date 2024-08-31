2fd3d0871d3dc52bf4f942a71b3b54191ce34b03
Madmegsox1 <madmegsox2@gmail.com>
2024-08-31 23:49:23 +0100

    [Addition] Started on listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use std::{env, error::Error, io::Result, net::UdpSocket};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    //
+    // TODO wright a args parser
+    //
+    if args.len() < 2 {
+        panic!("Error: No port specifed with -p")
+    }
+
+    if args[0] != "-p" {
+        panic!("Error: No port specifed with -p")
+    }
+
+    let port_wrapped = args[1].parse::<i32>();
+
+    match port_wrapped {
+        Ok(port) => {
+
+
+        }
+        Err(err) => {
+            panic!("Error: Typing to parse port got this error {}", err)
+
+        }
+
+    }
+}
+
+
+fn listen(port: i32) -> std::io::Result<()>{
+    {
+        let socket = UdpSocket::bind(format!("0.0.0.0:{}", port))?;
+
+        let mut buffer = [0; 400];
+
+        let (amt, src) = socket.recv_from(&mut buffer)?;
+
+        let buffer = &mut buffer[..amt];
+        buffer.reverse();
+
+        
+    }
+
+    Ok(())
+
 }
